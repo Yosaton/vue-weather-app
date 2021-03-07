3e3795cf0b48140ad2df9b3f4a1a754fd56423a1
Yosaton <y.tungmanelatkul@gmail.com>
2021-03-06 18:40:50 -0600

    basic functionality working + added vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,50 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <v-card raised>
+    HelloWorld
+  </v-card>
+<v-form @submit.prevent="apiCall">
+  <label for="fname">City:</label>
+  <input type="text" v-model="city" id="fname" name="fname">
+  <input
+    type="submit"
+    value="submit"
+  >
+</v-form>
+  <div v-if="this.weather.main != undefined">
+    <h1>{{ this.weather.main.temp}}</h1>
   </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import {apiKey} from './ApiKey.js';
+
 
 export default {
   name: 'App',
+  data(){
+    return{
+      weather: {},
+      city: '',
+    }
+  },
   components: {
-    HelloWorld
-  }
+    // HelloWorld
+  },
+  methods: {
+    apiCall(){
+      fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&APPID=${apiKey}&units=imperial`)
+        .then(res => res.json())
+        .then(data => this.weather = data)
+        .then(console.log(this.weather));
+      }
+    }
 }
+  
 </script>
 
 <style>
